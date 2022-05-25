182f285149a158702fce25aeca3e0792fa9ef32e
Alumno <Alumno@MarcosQuinteros>
2022-05-24 21:12:53 -0300

    Marcos Quinterso avance

--- src/main/java/ar/edu/unju/edm/model/Usuario.java
+++ src/main/java/ar/edu/unju/edm/model/Usuario.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.edm.model;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,15 +14,20 @@ public class Usuario {
 	private String nombre;
 	private String apellido;
 	private String email;
+	@NotEmpty //vacio string
 	private String contrasena;
+	private Boolean estado;
+	@NotNull //para numeros
+	@Min(value=1000000,message="El Dni debe ser mayor que millon")
+	@Max(value=99999999,message="El Dni debe ser menor que 9999999")
+	private Long dni;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate fechaNacimiento;
 	
 	public Usuario() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	
-	
-
 	public String getNombre() {
 		return nombre;
 	}
@@ -46,6 +58,30 @@ public class Usuario {
 
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public Long getDni() {
+		return dni;
+	}
+
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 	
 	
