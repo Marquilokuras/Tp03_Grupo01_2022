d574fcace07c22105a4e9e1f0afb2cc1adecd9da
Usuario <Usuario@DESKTOP-QEN7T49>
2022-06-07 09:55:29 -0300

    Marcos Quinteros:Agrego campos Tabla y arreglos en CursosController

--- src/main/java/ar/edu/unju/edm/controller/CursosController.java
+++ src/main/java/ar/edu/unju/edm/controller/CursosController.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ar.edu.unju.edm.model.Curso;
-import ar.edu.unju.edm.service.IUsuarioService;
+import ar.edu.unju.edm.service.ICursoService;
 import ar.edu.unju.edm.until.ListaCursos;
-
 
 @Controller
 public class CursosController {
@@ -25,103 +24,79 @@ public class CursosController {
 	Curso nuevoCurso;
 	
 	@Autowired
-	IUsuarioService serviceCurso;
+	ICursoService serviceCurso;
 	
 	@Autowired
 	ListaCursos lista;
 
 	@GetMapping("/otroCursos")//entra
 	public ModelAndView addCurso() {
-		ModelAndView vista = new ModelAndView("cargarCurso");//pasa nombre de la lista a pasar
-		//vista.addObject("nuevoUsuario");
+		ModelAndView vista = new ModelAndView("cargarCurso");
 		vista.addObject("curso", nuevoCurso);
 		vista.addObject("editMode", false);
 		return vista;
 	}
 
-	@PostMapping("/guardarCursos")//se recibe
+	@PostMapping("/guardarCursos")
 	public String saveCurso(@Valid  @ModelAttribute ("curso") Curso cursoparaguardar, BindingResult resultado, Model model) { //del modelo viene 1 atributo llamado usuario y lo agarra le indica el tipo y un nombre 
-		EMILIO.info("Ingresando a metodo guardar Curso: "+ cursoparaguardar.getFechaInicio() );
 		if(resultado.hasErrors()) {
-			EMILIO.fatal("Error de validacion");
+			EMILIO.fatal("Error de Validacion");
 			model.addAttribute("curso",cursoparaguardar);
 			return "cargarCurso";
 		}
-		lista.getListado().add(cursoparaguardar); //el user se guarda en listado
-		EMILIO.error("Tamaño del Listado: " + lista.getListado().size());
-		return "redirect:/otroCursos";
+		try { //controla si algo se ejecuta bien
+			serviceCurso.guardarCursos(cursoparaguardar);
+		}catch(Exception error){ //si no sale por aqui
+			model.addAttribute("formUsuarioErrorMessage", error.getMessage());
+			model.addAttribute("usuario",cursoparaguardar);
+			EMILIO.error("No se pudo guardar el curso");
+			return "cargarUsuario";
+		}
+		model.addAttribute("formCursoErrorMessage", "Curso Guardado Correctamente");
+		model.addAttribute("curso", nuevoCurso);
+		return "cargarCurso";
 	}
 
 	@GetMapping("/listadoCursos")
 	public ModelAndView showCursos() {
 		ModelAndView vista = new ModelAndView("listadoCursos");
-
 		vista.addObject("listaCursos", lista.getListado());
 		return vista;
 	}
 
-	@GetMapping("/editarCursos/{id}")
-	public ModelAndView editCurso(@PathVariable(name="id")Long idCurso) {
+	@GetMapping("/editarCursos/{idCurso}")
+	public ModelAndView editCurso(@PathVariable(name="idCurso")Long idCurso,Model model) {
 		Curso cursoEncontrado = new Curso();
-		for(int i=0;i<lista.getListado().size();i++) {
-			if(lista.getListado().get(i).getIdCurso().equals(idCurso)) {
-				cursoEncontrado = lista.getListado().get(i);
-			}
-		};
-		EMILIO.fatal("Error de entrada"+ cursoEncontrado.getIdCurso());
-		ModelAndView encontrado = new ModelAndView("cargarCurso");
-
-		encontrado.addObject("curso", cursoEncontrado);
-		encontrado.addObject("band", "true");
-		return encontrado;
+		try {
+			cursoEncontrado = serviceCurso.buscarCurso(idCurso);
+		}catch(Exception e){
+			model.addAttribute("formUsuarioErrorMessage", e.getMessage());
+		}
+		ModelAndView modelView = new ModelAndView("cargarCurso");
+		modelView.addObject("curso", cursoEncontrado);
+		EMILIO.error("curso: "+ cursoEncontrado.getIdCurso());
+		modelView.addObject("editMode", true);
+		return modelView;
 	}
 
 	@PostMapping("/modificarCurso")//se recibe
-	public String modCurso(@Valid  @ModelAttribute ("curso") Curso cursosparamodificar, BindingResult resultado, Model model) { //del modelo viene 1 atributo llamado usuario y lo agarra le indica el tipo y un nombre 
-		EMILIO.info("Ingresando al metodo guardar Curso: "+ cursosparamodificar.getFechaInicio() );
-		if(resultado.hasErrors()) {
-			EMILIO.fatal("Error de validacion");
-			model.addAttribute("curso",cursosparamodificar);
-			return "cargarCurso";
-		}
-		for(int i=0;i<lista.getListado().size();i++) {
-			if(lista.getListado().get(i).getIdCurso().equals(cursosparamodificar.getIdCurso())) {
-				lista.getListado().set(i, cursosparamodificar);
-			}
-		};
-
-		EMILIO.error("Tamaño del Listado: " + lista.getListado().size());
-		return "redirect:/listadoCursos";
+	public ModelAndView postEditarCurso(@ModelAttribute ("curso") Curso cursoparamodificar) {  
+		serviceCurso.modificarCurso(cursoparamodificar);
+		ModelAndView vista = new ModelAndView("listadoCursos");
+		vista.addObject("listaCursos", serviceCurso.mostrarCursos());
+		vista.addObject("formUsuarioErrorMessage", "Curso Guardado Correctamente");
+		return vista;
 	}
 	
-	@GetMapping("/eliminarCurso/{id}")
-	public ModelAndView deleteCurso(@PathVariable(name="id")Long id) {
-		Curso cursoEncontrado = new Curso();
-		for(int i=0;i<lista.getListado().size();i++) {
-			if(lista.getListado().get(i).getIdCurso().equals(id)) {
-				cursoEncontrado = lista.getListado().remove(i);
-			}
-		};
-		EMILIO.fatal("Error de entrada"+ cursoEncontrado.getIdCurso());
-		ModelAndView encontrado = new ModelAndView("cursoUsuario");
-		
-		encontrado.addObject("curso", cursoEncontrado);
-		encontrado.addObject("band", "true");
-		return encontrado;
-	}
-
-	@PostMapping("/sacarCurso")//se recibe
-	public String sacarCurso(@Valid  @ModelAttribute ("curso") Curso cursoparasacar, BindingResult resultado, Model model) { //del modelo viene 1 atributo llamado usuario y lo agarra le indica el tipo y un nombre 
-		EMILIO.info("Ingresando al metodo sacar Curso: "+ cursoparasacar.getFechaInicio() );
-		if(resultado.hasErrors()) {
-			EMILIO.fatal("Error de validacion");
-			model.addAttribute("curso",cursoparasacar);
-			return "cargarCurso";
+	@GetMapping("/eliminarCurso/{idCurso}")
+	public String deleteCurso(@PathVariable(name="idCurso")Long idCurso, Model model) {
+		try {
+			serviceCurso.eliminarCurso(idCurso);
+		}catch(Exception error){
+			EMILIO.error("No se pudo eliminar el curso");
+			model.addAttribute("formUsuarioErrorMessage", error.getMessage());
+			return "redirect:/otroCursos";
 		}
-		for(int i=0;i<lista.getListado().size();i++) {
-			if(lista.getListado().get(i).getIdCurso().equals(cursoparasacar.getIdCurso())) {
-			}
-		}
-		return "cargarCurso";
+		return "redirect:/listadoCursos";
 	}
 }
